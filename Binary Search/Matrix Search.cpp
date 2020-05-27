50535dc9c9b6f85e28bf5eccc119731d3f4a351c
sanjana Singh <sanjanasing214@gmail.com>
2020-05-27 20:23:16 +0530

    https://www.interviewbit.com/problems/matrix-search/

---
+++ Binary Search/Matrix Search.cpp
@@ -0,0 +1,33 @@
+int binarySearch(vector<int> A, int start, int end, int key){
+    int mid = start + (end-start)/2;
+    if(start <= end){
+        if(A[mid] == key){
+            return 1;
+        }
+        else if(A[mid] > key){
+            return binarySearch(A, start, mid-1, key);
+        }
+        else{  // A[mid] < key
+            return binarySearch(A, mid+1, end, key);
+        }
+    }
+    return 0;
+}
+
+int Solution::searchMatrix(vector<vector<int> > &A, int B) {
+    int rows = A.size();
+    int cols = A[0].size();
+    
+    for(int i = 0; i < rows; i++){
+        if((A[i][0] <= B) && (A[i][cols-1] >= B)){
+            if(binarySearch(A[i], 0, cols-1, B)){
+                return 1;
+            }
+            else{
+                return 0;
+            }
+        }
+    }
+    
+    return 0;
+}
