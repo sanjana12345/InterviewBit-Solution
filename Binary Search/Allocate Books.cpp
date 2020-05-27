716c2703e1b4b7f522c821e345450e0524fda9d8
sanjana Singh <sanjanasing214@gmail.com>
2020-05-27 20:20:47 +0530

    https://www.interviewbit.com/problems/allocate-books/

---
+++ Binary Search/Allocate Books.cpp
@@ -0,0 +1,52 @@
+bool isPossible(vector<int>&arr, int n, int m, int curr_min)
+{
+    int studentsRequired = 1;
+    int curr_sum = 0;
+     for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > curr_min)
+            return false;
+         if (curr_sum + arr[i] > curr_min)
+        {
+            studentsRequired++;
+ 
+            // update curr_sum
+            curr_sum = arr[i];
+     if (studentsRequired > m)
+                return false;
+        }
+         else
+            curr_sum += arr[i];
+    }
+    return true;
+}
+ int findPages(vector<int>&arr, int n, int m)
+{
+    long long sum = 0;
+     if (n < m)
+        return -1;
+     for (int i = 0; i < n; i++)
+        sum += arr[i];
+    int start = 0, end = sum;
+    int result = INT_MAX;
+    while (start <= end)
+    {
+        int mid = (start + end) / 2;
+        if (isPossible(arr, n, m, mid))
+        {
+            result = min(result, mid);
+            end = mid - 1;
+        }
+ 
+        else
+            start = mid + 1;
+    }
+ 
+    return result;
+}
+
+int Solution::books(vector<int> &A, int B) {
+    int n=A.size();
+    int s=findPages(A,n,B);
+    return s;
+}
