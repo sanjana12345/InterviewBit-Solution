0759706faffe7b77cbe5b854f9b82b6659a8d170
sanjana Singh <sanjanasing214@gmail.com>
2020-05-27 18:35:44 +0530

    https://www.interviewbit.com/problems/implement-strstr/

---
+++ String/Implement StrStr.cpp
@@ -0,0 +1,13 @@
+int Solution::strStr(const string haystack, const string needle) {
+            int m = haystack.length(), n = needle.length();
+        if (!n) return 0;
+        for (int i = 0; i < m - n + 1; i++) {
+            int j = 0;
+            for (; j < n; j++)
+                if (haystack[i + j] != needle[j])
+                    break;
+            if (j == n) return i;
+        }
+        return -1;
+
+}
