150dae8a30f335951d6720c18ce063004f0691e3
sanjana Singh <sanjanasing214@gmail.com>
2020-05-27 19:56:59 +0530

    https://www.interviewbit.com/problems/remove-duplicates-from-sorted-array-ii/

---
+++ Two Pointers/Remove Duplicates from Sorted Array II.cpp
@@ -0,0 +1,13 @@
+int Solution::removeDuplicates(vector<int> &A) {
+int n=A.size();
+            int count = 0;
+            for (int i = 0; i < n; i++) { 
+                if (i < n - 2 && A[i] == A[i+1] && A[i] == A[i+2]) continue;
+                else {
+                    A[count] = A[i];
+                    count++;
+                }
+            }
+            return count;
+        }  
+
