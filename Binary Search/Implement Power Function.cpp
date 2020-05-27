b0d26b84d0cadbc7757f482c722e5963dbab03d1
sanjana Singh <sanjanasing214@gmail.com>
2020-05-27 20:19:31 +0530

    https://www.interviewbit.com/problems/implement-power-function/

---
+++ Binary Search/Implement Power Function.cpp
@@ -0,0 +1,23 @@
+int Solution::pow(int x, int n, int d) {
+    if(x==0)
+        return 0;
+    if(n==0)
+        return 1;
+    long long a=1;
+    long long y=x%d;
+    if(y<0) 
+    y=y+d;
+    while(n>0){
+        if(n&1){
+            
+            a=(a*y)%d;
+        }
+        y=y*y;
+        y=y%d;
+        if(y<0)
+            y+=d;
+        n=n/2;
+    }
+    return (int)a;
+}
+
